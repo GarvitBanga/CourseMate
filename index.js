6e94359a19090a83a97bce097c832e46c715b20d
Garvit Banga <bangaboy181@gmail.com>
2025-03-17 18:02:46 -0400

    routes

--- index.js
+++ index.js
@@ -1,19 +1,45 @@
-const express = require("express");
+const express=require('express');
 const app = express();
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-
 app.use(express.json());
+const jwt = require('jsonwebtoken');
+const JWT_SECRET='JWT_SECRET';
+const { ObjectId } = require('mongodb');
+
+const mongoose=require('mongoose'); 
+const fs=require('fs');
+const path=require('path');
+const dblink=fs.readFileSync(path.join(__dirname,'../mongodblink'),'utf8');  
+mongoose.connect(dblink);
 
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post("/login", (req, res) => {
- 
+app.post("/user/signup", (req, res) => {
+ res.json({message:"Hello World"});
 
 });
+
+app.post("/user/signin", (req, res) => {
+    res.json({message:"Hello World"});
+   
+});
+
+
+app.get("/user/purchases", (req, res) => {
+    res.send("Hello World");
+});
+
+
+app.post("/course/purchase", (req, res) => {
+    res.send("Hello World");
+});
+           
+app.get("/courses", (req, res) => {
+  res.send("Hello World");
+});
+      
  
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
