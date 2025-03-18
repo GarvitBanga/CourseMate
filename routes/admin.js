7448d0b0586ef11bb3203796476b6dba571aa51e
Garvit Banga <bangaboy181@gmail.com>
2025-03-17 23:15:45 -0400

    admin and user signup and signin

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,86 @@
 const {Router}=require("express");
 const adminRouter=Router();
+const bcrypt=require('bcrypt');
+const { z } = require("zod");
+const {adminModel}=require('../db');
+const jwt=require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET_ADMIN=process.env.JWT_SECRET_ADMIN;
 
- 
-adminRouter.post("/signup", (req, res) => {
-    res.json({message:"Hello World"});
-   
+
+
+adminRouter.post("/signup", async (req, res) => {
+    const reqdbody=z.object({
+        email:z.string().email(),
+        firstName:z.string(),
+        lastName:z.string(),
+        password:z.string().min(8).max(30).refine((value)=>{
+            const specialCharRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\|"\-=`]/;
+            const numberRegex = /[0-9]/;
+            const lowerCaseRegex = /[a-z]/;
+            const upperCaseRegex = /[A-Z]/;
+            return specialCharRegex.test(value) && numberRegex.test(value) && lowerCaseRegex.test(value) && upperCaseRegex.test(value);
+        },{message:'Password must contain at least one special character and one number and one uppercase and lowercase letter'})
+    });
+    // const parseddata=reqdbody.parse(req.body); //throws error so we use safeParse
+    const parseddatasafe=reqdbody.safeParse(req.body); //this returns a object with success and error
+
+    if(!parseddatasafe.success){
+        res.json({
+            message:'Incorrect data format',
+            error:parseddatasafe.error
+        });
+        return;
+    }
+
+    
+    
+    
+    
+    const email=req.body.email;
+    const password=req.body.password;
+    const firstName=req.body.firstName;
+    const lastName=req.body.lastName;
+
+    let error=false;
+    try{
+        const hashedpass=await bcrypt.hash(password,10);
+        console.log(hashedpass);
+    await adminModel.create({
+        email:email,
+        firstName:firstName,
+        lastName:lastName,
+        password:hashedpass
+    });
+    }catch(err){
+        res.status(400).json({message:'Email already exists'});
+        error=true;
+    }
+
+    if(!error){
+        res.json({message:'Admin created successfully'});
+    }
 });
+
    
-adminRouter.post("/signin", (req, res) => {
-       res.json({message:"Hello World"});
+adminRouter.post("/signin", async(req, res) => {
+    const email=req.body.email;
+    const password=req.body.password;
+
+    const admin=await adminModel.findOne({
+        email:email
+    });
+    if(!admin){
+        res.status(400).json({message:'Admin not found'});
+    }
+    const found= await bcrypt.compare(password,admin.password); //need await as it returns a promise
+    console.log(found);
+    if(found){
+        const token=jwt.sign({id:admin._id.toString()},JWT_SECRET_ADMIN);
+        res.json({message:'You are logged in now',token:token});
+    }else{
+        res.status(400).json({message:'Incorrect Credentials'});
+    }
       
 });
    
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,82 @@
 const {Router}=require("express");
+const { userModel } = require("../db");
 const userRouter = Router();
+const bcrypt=require('bcrypt');
+const { z } = require("zod");
+const jwt=require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET_USER=process.env.JWT_SECRET_USER;
+// console.log(JWT_SECRET_USER);
 
- 
-userRouter.post("/signup", (req, res) => {
-    res.json({message:"Hello World"});
-   
+
+
+userRouter.post("/signup", async (req, res) => {
+    const {email,firstName,lastName,password}=req.body;
+
+    const reqdbody=z.object({
+        email:z.string().email(),
+        firstName:z.string(),
+        lastName:z.string(),
+        password:z.string().min(8).max(30).refine((value)=>{
+            const specialCharRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\|"\-=`]/;
+            const numberRegex = /[0-9]/;
+            const lowerCaseRegex = /[a-z]/;
+            const upperCaseRegex = /[A-Z]/;
+            return specialCharRegex.test(value) && numberRegex.test(value) && lowerCaseRegex.test(value) && upperCaseRegex.test(value);
+        },{message:'Password must contain at least one special character and one number and one uppercase and lowercase letter'})
+    });
+    // const parseddata=reqdbody.parse(req.body); //throws error so we use safeParse
+    const parseddatasafe=reqdbody.safeParse(req.body); //this returns a object with success and error
+
+    if(!parseddatasafe.success){
+        res.json({
+            message:'Incorrect data format',
+            error:parseddatasafe.error
+        });
+        return;
+    }
+    let error=false;
+    try{
+        const hashedpass=await bcrypt.hash(password,10);
+        console.log(hashedpass);
+        await userModel.create({
+            email:email,
+            firstName:firstName,
+            lastName:lastName,
+            password:hashedpass
+        });
+    }catch(err){
+        res.status(400).json({message:'Email already exists'});
+        error=true;
+    }
+
+    if(!error){
+        res.json({message:'User created successfully'});
+    }
+
+
+
+
 });
    
-userRouter.post("/signin", (req, res) => {
-       res.json({message:"Hello World"});
+userRouter.post("/signin", async (req, res) => {
+    const email=req.body.email;
+    const password=req.body.password;
+
+    const user=await userModel.findOne({
+        email:email
+    });
+    if(!user){
+        res.status(400).json({message:'User not found'});
+    }
+    const found= await bcrypt.compare(password,user.password); //need await as it returns a promise
+    console.log(found);
+    if(found){
+        const token=jwt.sign({id:user._id.toString()},JWT_SECRET_USER);
+        res.json({message:'You are logged in now',token:token});
+    }else{
+        res.status(400).json({message:'Incorrect Credentials'});
+    }
       
 });
    
